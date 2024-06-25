33f7246aa3e8c4c9a395f86581f6b34daa8e9ead
ntustanovskaya <ksaarrj@gmail.com>
2024-06-25 22:02:12 +0300

    KS-1-3 Gagarin

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,12 @@
+package lesson1
+
+fun main() {
+
+    val year: Int = 1961
+    var hour: String = "10:"
+    var minute: String = "55"
+
+    println(year)
+    print(hour)
+    println(minute)
+}
