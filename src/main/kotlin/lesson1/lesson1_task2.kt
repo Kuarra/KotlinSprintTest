c47f535796c348a82d7dc3490d42df18443f4359
ntustanovskaya <ksaarrj@gmail.com>
2024-06-25 21:38:21 +0300

    KS-1-2 workers

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package lesson1
+
+fun main() {
+
+    var numbersOfOrders: Int = 75
+    var notificationText: String = "Благодарим за покупку нашего товара!"
+    var numbersOfWorkers: Int = 1999
+    println(numbersOfOrders)
+    println(notificationText)
+    // println(numbersOfWorkers)
+    println(numbersOfWorkers)
+}
