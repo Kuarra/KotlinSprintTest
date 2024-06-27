747fa8836a19402aa8acc22b0a16745e8c7043b5
ntustanovskaya <ksaarrj@gmail.com>
2024-06-27 21:40:20 +0300

    KS-1-4 types refactor

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -2,9 +2,9 @@ package lesson1
 
 fun main() {
 
-    var numbersOfOrders: Int = 75
-    var notificationText: String = "Благодарим за покупку нашего товара!"
-    var numbersOfWorkers: Int = 1999
+    val numbersOfOrders: Int = 75
+    val notificationText: String = "Благодарим за покупку нашего товара!"
+    val numbersOfWorkers: Int = 1999
     println(numbersOfOrders)
     println(notificationText)
     // println(numbersOfWorkers)
