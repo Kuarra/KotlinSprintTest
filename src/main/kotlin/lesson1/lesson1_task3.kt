238b822de6cb45e8bd1cdd47251342e26f8f9b78
ntustanovskaya <ksaarrj@gmail.com>
2024-06-27 21:58:00 +0300

    KS-1-3 types refactor

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -3,10 +3,13 @@ package lesson1
 fun main() {
 
     val year: Int = 1961
-    var hour: String = "10:"
-    var minute: String = "55"
+    var hourDeparture: String = "09:"
+    var minuteDeparture: String = "07"
+    var hourArrival: String = "10"
+    var minuteArrival: String = "55"
 
     println(year)
-    print(hour)
-    println(minute)
+    print(hourDeparture)
+    println(minuteDeparture)
+    print("$hourArrival:$minuteArrival")
 }
